6c5812c544b4225065535aa2605cecb3d5a98c24
Paul Rau <paul.rau1@digital.homeoffice.gov.uk>
2021-04-20 17:21:44 +0100

    HOCS-2498 S3 SQS ES DB & Deployment Scripts Allowing for purging of all ES instances in hocs-delta, hocs-gamma & hocs-qax.

--- scripts/purge-es.sh
+++ scripts/purge-es.sh
@@ -4,8 +4,8 @@ set -euox pipefail
 export VALID_NAMESPACES=("hocs-gamma" "hocs-delta" "hocs-qax")
 
 current=$(kubectl config current-context)
-if [[ ${VALID_NAMESPACES[*]} =~ $current ]] ; then
-  echo "Current context is $current"
+if [[ ${VALID_NAMESPACES[*]} =~ ${current} ]] ; then
+  echo "Current context is ${current}"
 else
   echo "Unauthorised Namespace" >&2
   exit 1
@@ -18,13 +18,13 @@ read -p 'Are you sure you want to remove all the contents (y/n)? ' yes_no
 
 if [[ $yes_no = 'y' || $yes_no = 'Y' ]]
 then
-    aws-es-curl --profile not-prod-es --region=eu-west-2 -X DELETE https://search-cs-notprod-elasticsearch-eg6slvni45k55o2j3lmo6yupdy.eu-west-2.es.amazonaws.com/hocs-qax-case
-    aws-es-curl --profile not-prod-es --region=eu-west-2 -X DELETE https://search-cs-notprod-elasticsearch-eg6slvni45k55o2j3lmo6yupdy.eu-west-2.es.amazonaws.com/hocs-qax-lastest-case
+    aws-es-curl --profile not-prod-es --region=eu-west-2 -X DELETE https://search-cs-notprod-elasticsearch-eg6slvni45k55o2j3lmo6yupdy.eu-west-2.es.amazonaws.com/${current}-case
+    aws-es-curl --profile not-prod-es --region=eu-west-2 -X DELETE https://search-cs-notprod-elasticsearch-eg6slvni45k55o2j3lmo6yupdy.eu-west-2.es.amazonaws.com/${current}-lastest-case
 
-    aws-es-curl --profile not-prod-es --region=eu-west-2 -X PUT https://search-cs-notprod-elasticsearch-eg6slvni45k55o2j3lmo6yupdy.eu-west-2.es.amazonaws.com/hocs-qax-case \
+    aws-es-curl --profile not-prod-es --region=eu-west-2 -X PUT https://search-cs-notprod-elasticsearch-eg6slvni45k55o2j3lmo6yupdy.eu-west-2.es.amazonaws.com/${current}-case \
      -H "Content-Type: application/json" -d @elastic_mapping
     aws-es-curl --profile not-prod-es --region=eu-west-2 -X POST https://search-cs-notprod-elasticsearch-eg6slvni45k55o2j3lmo6yupdy.eu-west-2.es.amazonaws.com/_aliases \
-     -d '{"actions":[{"add":{"alias":"hocs-qax-latest-case","index":"hocs-qax-case"}}]}'
+     -d '{"actions":[{"add":{"alias":"${current}-latest-case","index":"${current}-case"}}]}'
 else
     echo 'Aborted. No action was performed.' >&2
 fi
