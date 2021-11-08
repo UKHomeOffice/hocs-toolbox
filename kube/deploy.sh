34703ed0c67d4135bd3ea5911585df1cd1d36f54
Liam MacPherson <11508628+LiamMacP@users.noreply.github.com>
2021-11-08 14:17:27 +0000

    HOCS-4110: improve toolbox deployment (#15)
    * HOCS-4110: update deployment.yaml
    
    Bring the deployment.yaml in line with what is present within the `kube`
     repository. This adds missing secrets that are required for the
     functionality of some scripts. This change also allows for the
     releasing of user specified versions of the image.
    
    * HOCS-4110: update tag
    
    Instead of defaulting to using a `branch-main` tag use latest as this is
     what branch-main will always be.
    
    * HOCS-4110: add not-prod deployment
    
    Add the ability to deploy the hocs-toolbox to all not-prod environments.
    
    * HOCS-4110: update deploy script
    
    As the deployment yaml has not been moved within a `kd` directory in
    line with other projects, this change has been reflected here.
    
    * HOCS-4110: add main branch step
    
    Currently, all pushes update the latest image, this change ensures only
    pushes to main trigger this.
    
    * Pass and add basic bash linter
    
    This adds a very basic defensive linter to the Drone pipeline to catch a
    load of confusing problems and Bash subteties we've no chance of
    catching ourselves.
    
    This commit moreover removes the broken purge ES script and makes a
    few stylistic changes to make the scripts less likely to break in
    strange Bash ways, to make the new linter pass:
    
    * added quotes around variables to prevent spaces in values from being
      interpreted as separate arguments
    * added -r argument to disallow escape characters to `read` builtin
    * changed syntax of the if or statements to one more portable
    * uppercased the default action in the prompts (this isn't a linting
      thing, just a personal preference)
    
    * HOCS-4110: update ssl cert auth
    
    You can now pass the certificate authority through the
    `KUBE_CERTIFICATE_AUTHORITY` variable instead of downloading the
    certificate.
    
    Co-authored-by: Douglas Gardner <douglas@chippy.ch>

--- kube/deploy.sh
+++ kube/deploy.sh
@@ -2,13 +2,15 @@
 set -euo pipefail # make bash quit if something weird happens
 
 export KUBE_NAMESPACE=${ENVIRONMENT}
-export REPLICAS="1"
-export CA_URL="https://raw.githubusercontent.com/UKHomeOffice/acp-ca/master/acp-notprod.crt"
+export VERSION=${VERSION:-latest}
+export KUBE_CERTIFICATE_AUTHORITY="https://raw.githubusercontent.com/UKHomeOffice/acp-ca/master/acp-notprod.crt"
 
-export KUBE_CERTIFICATE_AUTHORITY=/tmp/cert.crt
-if ! wget --quiet $CA_URL -O $KUBE_CERTIFICATE_AUTHORITY; then
-    echo "[error] failed to download certificate authority"
-    exit 1
-fi
+echo
+echo "Deploying to ${ENVIRONMENT}"
+echo "Version: ${VERSION}"
+echo
 
-kd --timeout 5m0s --file deployment.yaml
+cd kd
+
+kd --timeout 5m0s \
+  -f deployment.yaml
--- scripts/purge-s3.sh
+++ scripts/purge-s3.sh
@@ -2,11 +2,11 @@
 set -euo pipefail
 
 echo 'Warning: this action will completely remove all the documents in'
-echo 'both the trusted and untrusted S3 buckets of '${KUBE_NAMESPACE}'. It cannot be undone!'
+echo "both the trusted and untrusted S3 buckets of '${KUBE_NAMESPACE}'. It cannot be undone!"
 
-read -p 'Are you sure you want to remove all the documents (y/n)? ' yes_no
+read -rp 'Are you sure you want to remove all the documents (y/N)? ' yes_no
 
-if [ $yes_no = 'y' -o $yes_no = 'Y' ];
+if [ "$yes_no" = 'y' ] || [ "$yes_no" = 'Y' ];
 then
     export HTTP_PROXY=http://hocs-outbound-proxy.${KUBE_NAMESPACE}.svc.cluster.local:31290
     export HTTPS_PROXY=http://hocs-outbound-proxy.${KUBE_NAMESPACE}.svc.cluster.local:31290
@@ -15,13 +15,13 @@ then
     export AWS_SECRET_ACCESS_KEY=${S3_TRUSTED_SECRET_ACCESS_KEY}
     export AWS_DEFAULT_REGION=eu-west-2
 
-    aws s3 rm s3://${S3_TRUSTED_BUCKET} --recursive
+    aws s3 rm "s3://${S3_TRUSTED_BUCKET}" --recursive
 
     export AWS_ACCESS_KEY_ID=${S3_UNTRUSTED_ACCESS_KEY}
     export AWS_KMS_KEY_ID=${S3_UNTRUSTED_KMS_KEY}
     export AWS_SECRET_ACCESS_KEY=${S3_UNTRUSTED_SECRET_ACCESS_KEY}
 
-    aws s3 rm s3://${S3_UNTRUSTED_BUCKET} --recursive
+    aws s3 rm "s3://${S3_UNTRUSTED_BUCKET}" --recursive
 else
     echo 'Aborted. No action was performed.' >&2
 fi
