6cb3031a5161808d209c252f0ec50530892acfbf
Richard Weeks <richard.weeks1@digital.homeoffice.gov.uk>
2022-04-27 10:12:23 +0100

    HOCS-4970: Deploy to cs-dev-migration namespace.

--- kube/deploy.sh
+++ kube/deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -euo pipefail # make bash quit if something weird happens
 
-export KUBE_NAMESPACE=${DRONE_STEP_NAME}
+export KUBE_NAMESPACE=cs-dev-migration
 export VERSION=${VERSION:-latest}
 export KUBE_SERVER=https://kube-api-notprod.notprod.acp.homeoffice.gov.uk
 export KUBE_CERTIFICATE_AUTHORITY="https://raw.githubusercontent.com/UKHomeOffice/acp-ca/master/acp-notprod.crt"
@@ -13,5 +13,4 @@ echo
 
 cd kd
 
-kd --timeout 5m0s \
-  -f deployment.yaml
+kd --timeout 5m0s -f deployment.yaml
